6622ab152760906535d13a7805952b82104c3992
Tomás Correia Marques <tomasmanuelcorreiamarques@gmail.com>
2016-07-22 19:28:26 +0100

    push cenas

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -894,13 +894,12 @@ var qultivatorUser = new QultivatorUser({
     endpoint: 'ws://qultivator.whitesmith.co/ws/user'
 })
 .onData(function (data) {
-    console.log(data);
-    soilTemperatureChart.update(data.sT);
-    soilHumidityChart.update(data.sH);
-    environmentHumidityChart.update(data.eH);
-    environmentTemperatureChart.update(data.eT);
-    lightTemperatureChart.update(data.lT);
-    lightIntensityChart.update(data.lI);
+    p01.soilTemperatureChart.update(data.sT);
+    p01.soilHumidityChart.update(data.sH);
+    p01.environmentHumidityChart.update(data.eH);
+    p01.environmentTemperatureChart.update(data.eT);
+    p01.lightTemperatureChart.update(data.lT);
+    p01.lightIntensityChart.update(data.lI);
 });
 qultivatorUser.connect();
 
@@ -915,61 +914,73 @@ var defaultOptions = {
 var DISTANCELABELCENTER = 8;
 
 
+function drawCharts(plant_id, plant_var){
+    plant_var.soilTemperatureOptions = $.extend( {}, defaultOptions, {
+        max: 40,
+        series: [{ value: 0, color: ['#8BC34A', '#FF5722'] } ],
+        center: { content: [function (d) { return d.toFixed(0) + '°C' }, 'temperature','soil'], y: DISTANCELABELCENTER },
+    } );
+
+    plant_var.soilTemperatureChart = new RadialProgressChart(plant_id + '.soil_temperature', plant_var.soilTemperatureOptions);
+
+    plant_var.environmentHumidityOptions = $.extend( {}, defaultOptions, {
+        max: 100,
+        series: [ { value: 0, color: ['#8BC34A', '#FF5722'] } ],
+        center: { content: [function (d) { return d.toFixed(0) + '%' }, 'humidity','air'], y: DISTANCELABELCENTER },
+    } );
+
+    plant_var.environmentHumidityChart = new RadialProgressChart(plant_id + '.environment_humidity', plant_var.environmentHumidityOptions);
 
 
-var soilTemperatureOptions = $.extend( {}, defaultOptions, {
-    max: 40,
-    series: [{ value: 0, color: ['#8BC34A', '#FF5722'] } ],
-    center: { content: [function (d) { return d.toFixed(0) + '°C' }, 'temperature','soil'], y: DISTANCELABELCENTER },
-} );
+    plant_var.environmentTemperatureOptions = $.extend( {}, defaultOptions, {
+        max: 40,
+        series: [ { value: 0, color: ['#8BC34A', '#FF5722'] } ],
+        center: { content: [function (d) { return d.toFixed(0) + '°C' }, 'temperature', 'air' ], y: DISTANCELABELCENTER },
+    } );
 
-var soilTemperatureChart = new RadialProgressChart('.soil_temperature', soilTemperatureOptions);
-
-var environmentHumidityOptions = $.extend( {}, defaultOptions, {
-    max: 100,
-    series: [ { value: 0, color: ['#8BC34A', '#FF5722'] } ],
-    center: { content: [function (d) { return d.toFixed(0) + '%' }, 'humidity','air'], y: DISTANCELABELCENTER },
-} );
-
-var environmentHumidityChart = new RadialProgressChart('.environment_humidity', environmentHumidityOptions);
+    plant_var.environmentTemperatureChart = new RadialProgressChart(plant_id + '.environment_temperature', plant_var.environmentTemperatureOptions);
 
 
-var environmentTemperatureOptions = $.extend( {}, defaultOptions, {
-    max: 40,
-    series: [ { value: 0, color: ['#8BC34A', '#FF5722'] } ],
-    center: { content: [function (d) { return d.toFixed(0) + '°C' }, 'temperature', 'air' ], y: DISTANCELABELCENTER },
-} );
+    plant_var.lightTemperatureOptions = $.extend( {}, defaultOptions, {
+        max: 15000,
+        series: [ { value: 0, color: ['#8BC34A', '#FF5722'] } ],
+        center: { content: [function (d) { return d.toFixed(0) + 'K' }, 'temperature', 'light' ], y: DISTANCELABELCENTER },
+    } );
 
-var environmentTemperatureChart = new RadialProgressChart('.environment_temperature', environmentTemperatureOptions);
-
-
-var lightTemperatureOptions = $.extend( {}, defaultOptions, {
-    max: 15000,
-    series: [ { value: 0, color: ['#8BC34A', '#FF5722'] } ],
-    center: { content: [function (d) { return d.toFixed(0) + 'K' }, 'temperature', 'light' ], y: DISTANCELABELCENTER },
-} );
-
-var lightTemperatureChart = new RadialProgressChart('.light_temperature', lightTemperatureOptions);
+    plant_var.lightTemperatureChart = new RadialProgressChart(plant_id + '.light_temperature', plant_var.lightTemperatureOptions);
 
 
-var lightIntensityOptions = $.extend( {}, defaultOptions, {
-    max: 1000,
-    series: [ { value: 0, color: ['#8BC34A', '#FF5722'] } ],
-    center: { content: [function (d) { return d.toFixed(0) + 'lux' }, 'intensity', 'light' ], y: DISTANCELABELCENTER },
-} );
+    plant_var.lightIntensityOptions = $.extend( {}, defaultOptions, {
+        max: 1000,
+        series: [ { value: 0, color: ['#8BC34A', '#FF5722'] } ],
+        center: { content: [function (d) { return d.toFixed(0) + 'lux' }, 'intensity', 'light' ], y: DISTANCELABELCENTER },
+    } );
 
-var lightIntensityChart = new RadialProgressChart('.light_intensity', lightIntensityOptions);
-
-
-var soilHumidityOptions = $.extend( {}, defaultOptions, {
-    max: 100,
-    series: [ { value: 0, color: ['#8BC34A', '#FF5722'] } ],
-    center: { content: [function (d) { return d.toFixed(0) + '%' }, 'humidity', 'soil' ], y: DISTANCELABELCENTER },
-} );
+    plant_var.lightIntensityChart = new RadialProgressChart(plant_id + '.light_intensity', plant_var.lightIntensityOptions);
 
 
-var soilHumidityChart = new RadialProgressChart('.soil_humidity', soilHumidityOptions);
+    plant_var.soilHumidityOptions = $.extend( {}, defaultOptions, {
+        max: 100,
+        series: [ { value: 0, color: ['#8BC34A', '#FF5722'] } ],
+        center: { content: [function (d) { return d.toFixed(0) + '%' }, 'humidity', 'soil' ], y: DISTANCELABELCENTER },
+    } );
 
+
+    plant_var.soilHumidityChart = new RadialProgressChart(plant_id + '.soil_humidity', plant_var.soilHumidityOptions);
+
+    $(plant_id + '.water-icon').click(function(){
+        qultivatorUser.water("p01", 1);
+    })
+
+    $(plant_id + '.light-icon').click(function(){
+        qultivatorUser.light("p01", 1);
+    })
+}
+
+var p01={}, p02={}, p03={};
+drawCharts(".p01 ", p01);
+drawCharts(".p02 ", p02);
+drawCharts(".p03 ", p03);
 
 setTimeout(function(){
     $.each( $('.garden svg'), function( key, el ) {
